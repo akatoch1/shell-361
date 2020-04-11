37120916bd71719c5cc47f6ea30064cc8a9e1e2c
akatoch1 <44404876+akatoch1@users.noreply.github.com>
2020-04-10 20:37:56 -0400

    Add files via upload

--- sh.h
+++ sh.h
@@ -4,9 +4,10 @@
 int pid;
 int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
-char *where(char *command, struct pathelement *pathlist);
+void where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
-void printenv(char **envp);
-
+void print_env(int argcount, char **envp, char **args);
+void kill_proc(int argcount, char **args);
+char *print_prompt(char **args, int argcount);
 #define PROMPTMAX 32
 #define MAXARGS 10
